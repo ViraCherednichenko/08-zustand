12a020dcd32346681eddf63967a43f1a1bd66ae4
vira.cherednichenko <vera.cherednichenko88@gmail.com>
2026-01-23 22:54:25 +0200

    Fix

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -2,13 +2,15 @@ import NotesClient from "./Notes.client";
 import type { NoteTag } from "@/types/note";
 
 type PageProps = {
-  params: {
+  params: Promise<{
     slug?: string[];
-  };
+  }>;
 };
 
-export default function Page({ params }: PageProps) {
-  const rawTag = params.slug?.[0];
+export default async function Page({ params }: PageProps) {
+  const { slug } = await params;
+
+  const rawTag = slug?.[0];
 
   const tag: NoteTag | undefined =
     !rawTag || rawTag === "all" ? undefined : (rawTag as NoteTag);
--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+const BASE_URL = "https://notehub-public.goit.study/api";
+
+const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
 export const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : "",
+  },
 });
